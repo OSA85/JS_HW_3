7b25b94be090013e2a064c08708a9d56e4ab7628
Sergey Obidin <player181285@gmail.com>
2022-06-29 16:12:50 +0400

    added decision ex_3_3

---
+++ Task_3_3.js
@@ -0,0 +1,79 @@
+// 3. Написать функцию, которая будет добавлять предприятие. В качестве аргумента принимает название предприятия
+
+// Пример:
+// addEnterprise("Название нового предприятия")
+
+const enterprises = [
+    {
+      id: 1,
+      name: "Предприятие 1",
+      departments: [
+        {
+          id: 2,
+          name: "Отдел тестирования",
+          employees_count: 10,
+        },
+        {
+          id: 3,
+          name: "Отдел маркетинга",
+          employees_count: 20,
+        },
+        {
+          id: 4,
+          name: "Администрация",
+          employees_count: 15,
+        },
+      ]
+    },
+    {
+      id: 5,
+      name: "Предприятие 2",
+      departments: [
+        {
+          id: 6,
+          name: "Отдел разработки",
+          employees_count: 50,
+        },
+        {
+          id: 7,
+          name: "Отдел маркетинга",
+          employees_count: 20,
+        },
+        {
+          id: 8,
+          name: "Отдел охраны труда",
+          employees_count: 5,
+        },
+      ]
+    },
+    {
+      id: 9,
+      name: "Предприятие 3",
+      departments: [
+        {
+          id: 10,
+          name: "Отдел аналитики",
+          employees_count: 0,
+        },
+      ]
+    }
+  ]
+
+function addEnterprise(addEnterprise){
+    let maxId = 0;
+enterprises.forEach(enterpris => {
+    if (enterpris.id>maxId) maxId = enterpris.id;
+    enterpris.departments.forEach(department => {
+        if (department.id>maxId) maxId = department.id;
+        }
+        )
+    return maxId;
+    }
+)
+enterprises.push ({id: maxId + 1, name: addEnterprise, departments: []
+})
+console.log(enterprises)
+}
+
+
+addEnterprise("Предприятие 4")
