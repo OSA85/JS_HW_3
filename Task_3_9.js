0dfaf8181c29df77c6225f4b0c205143cb5b0f49
Sergey Obidin <player181285@gmail.com>
2022-06-30 15:16:24 +0400

    added Task_3_9

---
+++ Task_3_9.js
@@ -0,0 +1,76 @@
+// 9. Написать функцию для переноса сотрудников между отделами одного предприятия. В качестве аргумента принимает два значения: id отдела, из которого будут переноситься сотрудники и id отдела, в который будут переноситься сотрудники).
+
+// Пример:
+// moveEmployees(2, 3)
+
+const enterprises = [
+    {
+      id: 1,
+      name: "Предприятие 1",
+      departments: [
+        {
+          id: 2,
+          name: "Отдел тестирования",
+          employees_count: 10,
+        },
+        {
+          id: 3,
+          name: "Отдел маркетинга",
+          employees_count: 20,
+        },
+        {
+          id: 4,
+          name: "Администрация",
+          employees_count: 15,
+        },
+      ]
+    },
+    {
+      id: 5,
+      name: "Предприятие 2",
+      departments: [
+        {
+          id: 6,
+          name: "Отдел разработки",
+          employees_count: 50,
+        },
+        {
+          id: 7,
+          name: "Отдел маркетинга",
+          employees_count: 20,
+        },
+        {
+          id: 8,
+          name: "Отдел охраны труда",
+          employees_count: 5,
+        },
+      ]
+    },
+    {
+      id: 9,
+      name: "Предприятие 3",
+      departments: [
+        {
+          id: 10,
+          name: "Отдел аналитики",
+          employees_count: 0,
+        },
+      ]
+    }
+  ]
+
+
+function moveEmployees(idFrom, idWhere){
+    enterprises.forEach(enterprise => {
+        let departmentFrom = enterprise.departments.find (department => department.id ===idFrom)
+        let departmentWhere = enterprise.departments.find (department => department.id ===idWhere)
+        if(departmentFrom && departmentWhere){
+        departmentWhere.employees_count += departmentFrom.employees_count;
+        departmentFrom.employees_count = 0;}
+    })
+}
+
+
+
+moveEmployees(2, 3)
+console.log(enterprises[0])
