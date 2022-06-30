e32666e135dab597fe5e0378026622597186d10e
Sergey Obidin <player181285@gmail.com>
2022-06-30 12:02:38 +0400

    added Task_3_6

---
+++ Task_3_6.js
@@ -0,0 +1,71 @@
+// 6. Написать функцию для редактирования названия отдела. Принимает в качестве аргумента id отдела и новое имя отдела.
+
+// Пример:
+// editDepartment(7, "Новое название отдела")
+
+const enterprises = [
+    {
+      id: 1,
+      name: "Предприятие 1",
+      departments: [
+        {
+          id: 2,
+          name: "Отдел тестирования",
+          employees_count: 10,
+        },
+        {
+          id: 3,
+          name: "Отдел маркетинга",
+          employees_count: 20,
+        },
+        {
+          id: 4,
+          name: "Администрация",
+          employees_count: 15,
+        },
+      ]
+    },
+    {
+      id: 5,
+      name: "Предприятие 2",
+      departments: [
+        {
+          id: 6,
+          name: "Отдел разработки",
+          employees_count: 50,
+        },
+        {
+          id: 7,
+          name: "Отдел маркетинга",
+          employees_count: 20,
+        },
+        {
+          id: 8,
+          name: "Отдел охраны труда",
+          employees_count: 5,
+        },
+      ]
+    },
+    {
+      id: 9,
+      name: "Предприятие 3",
+      departments: [
+        {
+          id: 10,
+          name: "Отдел аналитики",
+          employees_count: 0,
+        },
+      ]
+    }
+  ]
+
+function editDepartment(idDepartment, newNameDepartment){
+    enterprises.find(enterpris => { let findDepartment = enterpris.departments.find(department => department.id === idDepartment)
+        if(findDepartment){ findDepartment.name = newNameDepartment;
+            console.log(findDepartment)
+            }
+         else {console.log(`В ${enterpris.name} нет отдела с id == ${idDepartment}`)}
+    });
+}
+
+editDepartment(7, "Новое название отдела")
