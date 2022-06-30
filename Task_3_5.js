e898999540087d74c91ed86246a147b2c656437e
Sergey Obidin <player181285@gmail.com>
2022-06-30 11:30:00 +0400

    added Task_3_5

---
+++ Task_3_5.js
@@ -0,0 +1,72 @@
+// 5. Написать функцию для редактирования названия предприятия. Принимает в качестве аргумента id предприятия и новое имя предприятия.
+
+// Пример:
+// editEnterprise(1, "Новое название предприятия")
+
+const enterprises = [
+    {
+      id: 1,
+      name: "Предприятие 1",
+      departments: [
+        {
+          id: 2,
+          name: "Отдел тестирования",
+          employees_count: 10,
+        },
+        {
+          id: 3,
+          name: "Отдел маркетинга",
+          employees_count: 20,
+        },
+        {
+          id: 4,
+          name: "Администрация",
+          employees_count: 15,
+        },
+      ]
+    },
+    {
+      id: 5,
+      name: "Предприятие 2",
+      departments: [
+        {
+          id: 6,
+          name: "Отдел разработки",
+          employees_count: 50,
+        },
+        {
+          id: 7,
+          name: "Отдел маркетинга",
+          employees_count: 20,
+        },
+        {
+          id: 8,
+          name: "Отдел охраны труда",
+          employees_count: 5,
+        },
+      ]
+    },
+    {
+      id: 9,
+      name: "Предприятие 3",
+      departments: [
+        {
+          id: 10,
+          name: "Отдел аналитики",
+          employees_count: 0,
+        },
+      ]
+    }
+  ]
+
+function editEnterprise (idEnterpris, newNameEnterpris){
+    let findEnterpris = enterprises.find(enterpris => enterpris.id === idEnterpris);
+    if (findEnterpris){
+        findEnterpris.name = newNameEnterpris;
+        console.log(findEnterpris)
+    }
+        else {console.log(`Нет предприятия с id == ${idEnterpris}`)}
+}
+
+
+  editEnterprise(1, 'Новое название предприятия')
